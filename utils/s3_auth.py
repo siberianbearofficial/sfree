5224bad0fdb4a73ad6b4d617c65f9d09057314e8
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-03-16 00:35:58 +0300

    test auth

--- utils/s3_auth.py
+++ utils/s3_auth.py
@@ -3,12 +3,11 @@ import hmac
 
 from typing import Annotated
 from fastapi import Depends, Path, Request
-from loguru import logger
 from pydantic import BaseModel
 
 from buckets.schema import BucketRead
 from utils.dependency import UOWDep, BucketServiceDep
-from utils.exceptions import exception_handler, AuthenticationError
+from utils.exceptions import exception_handler, AuthenticationError, NotFoundError
 
 
 class S3AuthenticatedRequest(BaseModel):
@@ -26,7 +25,6 @@ async def get_s3_authenticated_request(
     bucket_key: BucketKeyDep,
     request: Request,
 ) -> S3AuthenticatedRequest:
-    # превращаем любую ошибку в AuthenticationError для безопасности
     try:
         payload = await request.body()
 
@@ -38,19 +36,18 @@ async def get_s3_authenticated_request(
             supported_schemas=[aws4.AWSAuthSchema],
         )
         if not challenge.access_key_id:
-            raise RuntimeError("Empty access key")
+            raise ValueError("Empty access key")
 
         bucket = await bucket_service.get_bucket_by_access_key(
             uow, access_key=challenge.access_key_id
         )
         # сравниваем строки через hmac, чтобы по времени работы кода нельзя было понять, что произошло
         if not hmac.compare_digest(bucket.key, bucket_key):
-            raise RuntimeError(f"Bucket key does not match: {bucket.key, bucket_key}")
+            raise ValueError(f"Bucket key does not match: {bucket.key, bucket_key}")
 
         aws4.validate_challenge(challenge, bucket.access_secret)
-    except Exception as e:
-        # снаружи мы не поймем, что именно ломается, если будут баги, поэтому добавил лог внутри
-        logger.error(e)
+    except (aws4.AWS4Exception, ValueError, NotFoundError):
+        # превращаем любую ошибку в AuthenticationError для безопасности
         raise AuthenticationError
 
     return S3AuthenticatedRequest(
