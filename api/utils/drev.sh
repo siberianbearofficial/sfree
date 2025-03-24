c1003935956fe92829e6162ef72543e89a8ab6b1
Kozin Mikhail <kmv22u923@student.bmstu.ru>
2025-03-24 14:12:42 +0300

    change make targets

--- api/utils/drev.sh
+++ api/utils/drev.sh
@@ -1,7 +1,1 @@
-DB_HOST=localhost DB_PORT=5432 DB_NAME=postgres DB_USER=postgres DB_PASS=postgres alembic -c migrations/alembic.ini downgrade "$1"
-
-if [ $? -ne 0 ]; then
-    echo -e "\n---------------------\n"
-    echo "execute LOCAL_INIT=1 if you want to run on local config from local.env"
-    echo -e "\n---------------------\n"
-fi
+alembic -c migrations/alembic.ini downgrade "$1"
