69619c0cc451c1ae8a853d4e55a195e48d7222dc
Aleksei Orlov <61984103+siberianbearofficial@users.noreply.github.com>
2025-08-27 22:17:09 +0300

    fix(ui): configure HeroUI styles (#52)

--- webui/tailwind.config.ts
+++ webui/tailwind.config.ts
@@ -5,9 +5,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {extend: {}},
+  theme: {
+    extend: {}
+  },
   darkMode: "class",
   plugins: [heroui()]
 } satisfies Config;
