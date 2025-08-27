8d12cc26137bdb54345aa5c205f14b803d73e2ec
Aleksei Orlov <61984103+siberianbearofficial@users.noreply.github.com>
2025-08-27 21:48:20 +0300

    chore(webui): apply ui kit fonts

--- webui/tailwind.config.ts
+++ webui/tailwind.config.ts
@@ -5,9 +5,15 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {extend: {}},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      }
+    }
+  },
   darkMode: "class",
   plugins: [heroui()]
 } satisfies Config;
