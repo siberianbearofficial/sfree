e974d1b406c47fa7adb84be0970aa19abd19c3b4
Aleksei Orlov <61984103+siberianbearofficial@users.noreply.github.com>
2025-08-30 01:29:24 +0300

    feat(webui): scaffold dashboard page

--- webui/src/app/index.tsx
+++ webui/src/app/index.tsx
@@ -1,5 +1,14 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "../pages/landing";
+import DashboardPage from "../pages/dashboard";
 
 export default function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
